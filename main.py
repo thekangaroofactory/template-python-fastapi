771de3627baaa3227ba27c70b82f445c6def80fb
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2023-07-12 20:05:46 +0200

    Implement API

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+from fastapi import FastAPI
+
+# -- define the app
+my_api = FastAPI()
+
+
+# -- endpoint: get on /
+@my_api.get("/")
+async def info():
+    return {"message": "Welcome to my_api",
+            "version": "0.0.900"}
+
+
+# -- endpoint: get on /item
+@my_api.get("/item")
+async def item():
+    return {"item_id": "0001",
+            "item_name": "name"}
